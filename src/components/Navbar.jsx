a27f7a6780cd12e8454cfbca222d88eb33c3125e
eunbi-kang <ebzzang1905@naver.com>
2024-05-09 17:59:52 +0900

    Header styling Using tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Link }from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs'
 
 export default function Navbar() {
     return (
-        <header>
-            <Link to='/'>
+        <header className='flex justify-between border-b border-gray-300 p-2'>
+            <Link to='/' className='flex items-center text-4xl text-brand'>
                 <FiShoppingBag />
+                <h1>HaPpyShoPpY</h1>
             </Link>
-            <nav>
+            <nav className='flex items-center gap-4 font-semibold'>
                 <Link to='/products'>Products</Link>
                 <Link to='/carts'>Carts</Link>
-                <Link to='/products/new'>
+                <Link to='/products/new' className='text-2xl'>
                     <BsFillPencilFill />
                 </Link>
                 <button>Login</button>
