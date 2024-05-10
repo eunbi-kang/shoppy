c233412febaad38ff193593735ad4a22300943f2
eunbi-kang <ebzzang1905@naver.com>
2024-05-10 15:51:27 +0900

    Admin User - UI 완성

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs'
 import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
+import Button from './ui/Button';
 
 export default function Navbar() {
     const [user, setUser] = useState();
@@ -27,8 +28,8 @@ export default function Navbar() {
                     <BsFillPencilFill />
                 </Link>)}
                 {user && <User user={user} />}
-                {!user && <button onClick={login}>Login</button >}
-                {user && <button onClick={logout}>Logout</button >}
+                {!user && <Button text={'Login'} onClick={login}>Login</Button >}
+                {user && <Button text={'Logout'} onClick={logout}>Logout</Button >}
             </nav>
         </header>
     );
