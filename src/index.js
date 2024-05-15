7ba88501d0e7b99912f0b55df66646ceae8ef262
eunbi-kang <ebzzang1905@naver.com>
2024-05-15 22:34:03 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         <NewProduct /></ProtectedRoute>) },
       { path: '/products/:id', element: <ProductDetail /> },
       {
-        path: '/carts', element: (<ProtectedRoute>
+        path: '/carts', element: (<ProtectedRoute> 
           <MyCart /></ProtectedRoute>) 
       },
     ]
