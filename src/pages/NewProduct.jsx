714097bb0323986401c7fe88d8bb6a86aa57c8e6
eunbi-kang <ebzzang1905@naver.com>
2024-05-17 12:55:48 +0900

    image 선택할 수 있는 input file Form + 제품명 검색 input text form 추가

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function NewProduct() {
-    return (
-        <div>NewProduct</div>
-    );
+    const [product, setProduct] = useState({});
+    const [file, setFile] = useState();
+    const handleChange = (e) => {
+        const { name, value, files } = e.target;
+        if (name === 'file') {
+            setFile(files && files[0]);
+            return;
+        }
+        setProduct((product) => ({ ...product, [name]: value }));
+    };
+    const handleSubmit = (e) => { };
+    return <section>
+        <form onSubmit={handleSubmit}>
+            <input type="file" accept='image/*' name='file' required
+                onChange={handleChange} />
+            <input type='text' name='title' value={product.title ?? ''} placeholder='제품명' required onChange={handleChange} />
+        </form>
+    </section>
+
 }
 
