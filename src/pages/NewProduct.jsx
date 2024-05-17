11fe5b04e1601aadd28ceb1f6a25a57e163c7690
eunbi-kang <ebzzang1905@naver.com>
2024-05-17 13:15:01 +0900

    제품추가 전체 Form 완성 + 이미지 파일 추가되도록

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Button from '../components/ui/Button';
 
 export default function NewProduct() {
     const [product, setProduct] = useState({});
@@ -11,14 +12,61 @@ export default function NewProduct() {
         }
         setProduct((product) => ({ ...product, [name]: value }));
     };
-    const handleSubmit = (e) => { };
-    return <section>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // 제품의 사진을 Cloudinary에 업로드 하고 URL을 획득
+        // Firebase에 새로운 제품을 추가함
+
+    };
+    return (
+    <section>
+        {file && <img src={URL.createObjectURL(file)} alt='local file' />} 
         <form onSubmit={handleSubmit}>
             <input type="file" accept='image/*' name='file' required
                 onChange={handleChange} />
-            <input type='text' name='title' value={product.title ?? ''} placeholder='제품명' required onChange={handleChange} />
+            <input 
+                type='text' 
+                name='title' 
+                value={product.title ?? ''} 
+                placeholder='제품명' 
+                required 
+                onChange={handleChange} 
+            />
+            <input
+                type='number'
+                name='price' 
+                value={product.price ?? ''}
+                placeholder='가격'
+                required
+                onChange={handleChange} 
+            />
+            <input
+                type='text'
+                name='category' 
+                value={product.category ?? ''}
+                placeholder='카테고리'
+                required
+                onChange={handleChange} 
+            />
+            <input
+                type='text'
+                name='description' 
+                value={product.description ?? ''}
+                placeholder='제품 설명'
+                required
+                onChange={handleChange} 
+            />
+            <input
+                type='text'
+                name='options' 
+                value={product.options ?? ''}
+                placeholder='옵션들(콤마,)로 구분)'
+                required
+                onChange={handleChange} 
+            />
+            <Button text={'제품 등록하기'} />
         </form>
     </section>
-
+    )
 }
 
