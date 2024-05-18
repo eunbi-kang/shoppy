db01d800bb00fbe93488f984c7562a4acc87849f
eunbi-kang <ebzzang1905@naver.com>
2024-05-18 13:58:35 +0900

    401 Error 해결(.env파일 Prefix, Url 수정)

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -1,9 +1,7 @@
 export async function uploadImage(file) {
     const data = new FormData();
+    data.append('file', file);
     data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
-    console.log(process.env.REACT_APP_CLOUDINARY_PRESET);
-    console.log(process.env.REACT_APP_CLOUDINARY_URL);
-    console.log(process.env.REACT_APP_CLOUDINARY_TEST);
     return fetch(process.env.REACT_APP_CLOUDINARY_URL, {
         method: 'POST',
         body: data,
