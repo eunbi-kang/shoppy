8f3a6140c36df03ac0a7b0c0dbb45e4633a971f2
eunbi-kang <ebzzang1905@naver.com>
2024-05-18 23:14:45 +0900

    제품 상세 페이지 만들기

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 export default function ProductDetail() {
-    return (
-        <div>ProductDetail</div>
-    );
+    const {
+        state: {
+            product: {id, image, title, description, category, price, options} 
+        }
+    } = useLocation();
+    const [selected, setSelected] = useState(options && options[0])
+    const handleSelect = (e) => {setSelected(e.target.value)}
+    const handleClick = (e) => { }
+    return <section>
+        <p>{category}</p>
+        <img src={image} alt={title} />
+        <div>
+            <h2>{title}</h2>
+            <p>₩{price.toLocaleString()}</p>
+            <p>{description}</p>
+            <p>옵션: </p>
+            <select onChange={handleSelect} value ={selected}>
+                {options && options.map((option, index)=> <option key={index}>
+                    {option}
+                </option> )}
+            </select>
+            {/* 14.22 4:23 */}
+            <Button text='장바구니에 추가' onClick={handleClick} />
+        </div>
+    </section>
 }
 
